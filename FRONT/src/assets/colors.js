9e70a81b1e9eafdfe6b9576576459a695dd204c5
Git-Shira <shirae447@gmail.com>
2025-01-16 14:28:31 +0200

    Added colors.js for managing event colors

---
+++ FRONT/src/assets/colors.js
@@ -0,0 +1,33 @@
+const colors = [
+    { name: "Dark Red", value: "#9c191b" },
+    { name: "Bright Red", value: "#dd2c2f" },
+
+    { name: "Soft Pink", value: "#e05780" },
+    { name: "Light Pink", value: "#ff9ebb" },
+
+    { name: "Vibrant Orange", value: "#ff9505" },
+    { name: "Soft Orange", value: "#f7b267" },
+
+    { name: "Golden Yellow", value: "#ffd819" },
+    { name: "Soft Yellow", value: "#fae588" },
+
+    { name: "Dark Green", value: "#40916c" },
+    { name: "Light Green", value: "#95d5b2" },
+
+    { name: "Deep Turquoise", value: "#006d77" },
+    { name: "Light Turquoise", value: "#83c5be" },
+
+    { name: "Deep Blue", value: "#0077b6" },
+    { name: "Pale Blue", value: "#90e0ef" },
+
+    { name: "Soft Purple", value: "#9b72cf" },
+    { name: "Lavender Purple", value: "#c8b1e4" },
+
+    { name: "Warm Brown", value: "#b08968" },
+    { name: "Light Brown", value: "#e6ccb2" },
+
+    { name: "Charcoal Gray", value: "#6c757d" },
+    { name: "Light Gray", value: "#ced4da" },
+];
+
+export default colors;
